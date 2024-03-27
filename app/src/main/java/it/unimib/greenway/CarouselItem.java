f2803934313ab54e84d62c64a771aab406eb21ae
Davide-Domenico-Montani <d.montani1@campus.unimib.it>
2024-03-27 17:40:07 +0100

    finito xml welcome

---
+++ app/src/main/java/it/unimib/greenway/CarouselItem.java
@@ -0,0 +1,19 @@
+package it.unimib.greenway;
+
+public class CarouselItem {
+    private int imageResId;
+    private String text;
+
+    public CarouselItem(int imageResId, String text) {
+        this.imageResId = imageResId;
+        this.text = text;
+    }
+
+    public int getImageResId() {
+        return imageResId;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
