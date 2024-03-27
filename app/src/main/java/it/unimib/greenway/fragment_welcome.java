4bd7c87951ce963cab2d2b2bdfa29632834922ad
Leo24 <leonardo.minotti1999@gmail.com>
2024-03-27 17:44:27 +0100

    login e sign in fragment fatti

--- app/src/main/java/it/unimib/greenway/fragment_welcome.java
+++ app/src/main/java/it/unimib/greenway/fragment_welcome.java
@@ -2,11 +2,15 @@ package it.unimib.greenway;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +27,8 @@ public class fragment_welcome extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    Button loginButton, signInButton;
 
     public fragment_welcome() {
         // Required empty public constructor
@@ -51,7 +57,28 @@ public class fragment_welcome extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_welcome, container, false);
     }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        loginButton = view.findViewById(R.id.button_login);
+        signInButton = view.findViewById(R.id.button_signin);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(v).navigate(R.id.action_fragment_welcome_to_login);
+            }
+        });
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(v).navigate(R.id.action_fragment_welcome_to_sign_in);
+            }
+        });
+    }
+
+
 }
