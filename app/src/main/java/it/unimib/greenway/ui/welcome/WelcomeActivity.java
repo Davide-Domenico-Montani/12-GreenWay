ac854f7b8aaefd463bb84bf8b4326f1c5313507f
Leo24 <leonardo.minotti1999@gmail.com>
2024-04-03 18:43:14 +0200

    modifica colori status bar

--- app/src/main/java/it/unimib/greenway/ui/welcome/WelcomeActivity.java
+++ app/src/main/java/it/unimib/greenway/ui/welcome/WelcomeActivity.java
@@ -17,10 +17,6 @@ public class WelcomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_welcome);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.welcome), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
 }
