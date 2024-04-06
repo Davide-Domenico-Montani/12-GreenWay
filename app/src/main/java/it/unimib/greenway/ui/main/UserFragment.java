cd4aa98ee65f927ba347d0bf1843bead6fe96a68
Davide-Domenico-Montani <d.montani1@campus.unimib.it>
2024-04-06 18:34:49 +0200

    inizio grafica pagina user

--- app/src/main/java/it/unimib/greenway/ui/main/UserFragment.java
+++ app/src/main/java/it/unimib/greenway/ui/main/UserFragment.java
@@ -8,10 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.progressindicator.LinearProgressIndicator;
+
 import it.unimib.greenway.R;
 
 
 public class UserFragment extends Fragment {
+
+    private LinearProgressIndicator linearProgress;
 
     public UserFragment() {
         // Required empty public constructor
@@ -30,7 +34,11 @@ public class UserFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_user, container, false);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user, container, false);
+
+
+        return view;
     }
 }
