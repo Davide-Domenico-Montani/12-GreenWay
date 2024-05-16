2b458e4937b579e621667ae1eb67f3d1ad1fe310
Leonardo-Minotti <l.minotti3@campus.unimib.it>
2024-05-16 18:12:45 +0200

    ChallengeFragment e route Test

--- app/src/androidTest/java/it/unimib/greenway/ui/main/NavigatorFragmentTest.java
+++ app/src/androidTest/java/it/unimib/greenway/ui/main/NavigatorFragmentTest.java
@@ -56,7 +56,7 @@ public class NavigatorFragmentTest {
     public  void Login() throws InterruptedException {
 
             ViewInteraction materialButton = onView(
-                    allOf(withId(R.id.button_login), withText("Login"),
+                    allOf(withId(R.id.button_login), withText(R.string.login),
                             childAtPosition(
                                     allOf(withId(R.id.button_layout),
                                             childAtPosition(
@@ -103,7 +103,7 @@ public class NavigatorFragmentTest {
 
     @Test
     public void correcAddresstSearch() throws InterruptedException {
-        Thread.sleep(20000);
+        Thread.sleep(2000);
         ViewInteraction bottomNavigationItemView = onView(
                 allOf(withId(R.id.item_nav), withContentDescription("Navigatore"),
                         childAtPosition(
@@ -190,7 +190,7 @@ public class NavigatorFragmentTest {
 
     @Test
     public void errorAddressSearch() throws InterruptedException {
-        Thread.sleep(20000);
+        Thread.sleep(2000);
         ViewInteraction bottomNavigationItemView = onView(
                 allOf(withId(R.id.item_nav), withContentDescription("Navigatore"),
                         childAtPosition(
@@ -242,6 +242,7 @@ public class NavigatorFragmentTest {
                                 2),
                         isDisplayed()));
         materialButton2.perform(click());
+        Thread.sleep(2000);
 
         ViewInteraction frameLayout = onView(
                 allOf(withParent(allOf(withId(R.id.fragment_container_main),
@@ -250,7 +251,281 @@ public class NavigatorFragmentTest {
         frameLayout.check(matches(isDisplayed()));
 
     }
-        private static Matcher<View> childAtPosition(
+    @Test
+    public void navigatorRoutesRecyclerViewsTest() throws InterruptedException {
+        ViewInteraction bottomNavigationItemView = onView(
+                allOf(withId(R.id.item_nav), withContentDescription("Navigatore"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.bottom_navigation),
+                                        0),
+                                1),
+                        isDisplayed()));
+        bottomNavigationItemView.perform(click());
+
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(com.google.android.libraries.places.R.id.places_autocomplete_search_input),
+                        childAtPosition(
+                                allOf(withId(R.id.autocomplete_fragment),
+                                        childAtPosition(
+                                                withClassName(is("androidx.cardview.widget.CardView")),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        appCompatEditText.perform(click());
+        Thread.sleep(2000);
+
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(com.google.android.libraries.places.R.id.places_autocomplete_search_bar),
+                        childAtPosition(
+                                allOf(withId(com.google.android.libraries.places.R.id.places_autocomplete_search_bar_container),
+                                        childAtPosition(
+                                                withId(com.google.android.libraries.places.R.id.places_autocomplete_content),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        appCompatEditText2.perform(replaceText("Seregno"), closeSoftKeyboard());
+        Thread.sleep(2000);
+
+        ViewInteraction recyclerView = onView(
+                allOf(withId(com.google.android.libraries.places.R.id.places_autocomplete_list),
+                        childAtPosition(
+                                withId(com.google.android.libraries.places.R.id.places_autocomplete_content),
+                                3)));
+        recyclerView.perform(actionOnItemAtPosition(0, click()));
+        Thread.sleep(2000);
+
+        ViewInteraction appCompatEditText3 = onView(
+                allOf(withId(com.google.android.libraries.places.R.id.places_autocomplete_search_input),
+                        childAtPosition(
+                                allOf(withId(R.id.autocomplete_fragment2),
+                                        childAtPosition(
+                                                withClassName(is("androidx.cardview.widget.CardView")),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        appCompatEditText3.perform(click());
+        Thread.sleep(2000);
+
+        ViewInteraction appCompatEditText4 = onView(
+                allOf(withId(com.google.android.libraries.places.R.id.places_autocomplete_search_bar),
+                        childAtPosition(
+                                allOf(withId(com.google.android.libraries.places.R.id.places_autocomplete_search_bar_container),
+                                        childAtPosition(
+                                                withId(com.google.android.libraries.places.R.id.places_autocomplete_content),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        appCompatEditText4.perform(click());
+
+        ViewInteraction appCompatEditText5 = onView(
+                allOf(withId(com.google.android.libraries.places.R.id.places_autocomplete_search_bar),
+                        childAtPosition(
+                                allOf(withId(com.google.android.libraries.places.R.id.places_autocomplete_search_bar_container),
+                                        childAtPosition(
+                                                withId(com.google.android.libraries.places.R.id.places_autocomplete_content),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        appCompatEditText5.perform(replaceText("Viale zara"), closeSoftKeyboard());
+        Thread.sleep(2000);
+
+        ViewInteraction recyclerView2 = onView(
+                allOf(withId(com.google.android.libraries.places.R.id.places_autocomplete_list),
+                        childAtPosition(
+                                withId(com.google.android.libraries.places.R.id.places_autocomplete_content),
+                                3)));
+        recyclerView2.perform(actionOnItemAtPosition(0, click()));
+
+        ViewInteraction materialButton3 = onView(
+                allOf(withId(R.id.buttonFind), withText("Cerca"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.fragment_container_main),
+                                        0),
+                                2),
+                        isDisplayed()));
+        materialButton3.perform(click());
+        Thread.sleep(5000);
+
+        ViewInteraction recyclerView3 = onView(
+                allOf(withId(R.id.recyclerViewRoutes),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class))),
+                        isDisplayed()));
+        recyclerView3.check(matches(isDisplayed()));
+
+        ViewInteraction tabView = onView(
+                allOf(childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.tabLayout),
+                                        0),
+                                1),
+                        isDisplayed()));
+        tabView.perform(click());
+        Thread.sleep(2000);
+
+        ViewInteraction recyclerView4 = onView(
+                allOf(withId(R.id.recyclerViewRoutes),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class))),
+                        isDisplayed()));
+        recyclerView4.check(matches(isDisplayed()));
+        Thread.sleep(2000);
+
+        ViewInteraction tabView2 = onView(
+                allOf(childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.tabLayout),
+                                        0),
+                                2),
+                        isDisplayed()));
+        tabView2.perform(click());
+        Thread.sleep(2000);
+
+        ViewInteraction recyclerView5 = onView(
+                allOf(withId(R.id.recyclerViewRoutes),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class))),
+                        isDisplayed()));
+        recyclerView5.check(matches(isDisplayed()));
+    }
+
+    /*
+    @Test
+    public void navigatorRouteViewTest() throws InterruptedException {
+        ViewInteraction bottomNavigationItemView = onView(
+                allOf(withId(R.id.item_nav), withContentDescription("Navigatore"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.bottom_navigation),
+                                        0),
+                                1),
+                        isDisplayed()));
+        bottomNavigationItemView.perform(click());
+
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(com.google.android.libraries.places.R.id.places_autocomplete_search_input),
+                        childAtPosition(
+                                allOf(withId(R.id.autocomplete_fragment),
+                                        childAtPosition(
+                                                withClassName(is("androidx.cardview.widget.CardView")),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        appCompatEditText.perform(click());
+        Thread.sleep(2000);
+
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(com.google.android.libraries.places.R.id.places_autocomplete_search_bar),
+                        childAtPosition(
+                                allOf(withId(com.google.android.libraries.places.R.id.places_autocomplete_search_bar_container),
+                                        childAtPosition(
+                                                withId(com.google.android.libraries.places.R.id.places_autocomplete_content),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        appCompatEditText2.perform(replaceText("Seregno"), closeSoftKeyboard());
+        Thread.sleep(2000);
+
+        ViewInteraction recyclerView = onView(
+                allOf(withId(com.google.android.libraries.places.R.id.places_autocomplete_list),
+                        childAtPosition(
+                                withId(com.google.android.libraries.places.R.id.places_autocomplete_content),
+                                3)));
+        recyclerView.perform(actionOnItemAtPosition(0, click()));
+
+        ViewInteraction appCompatEditText3 = onView(
+                allOf(withId(com.google.android.libraries.places.R.id.places_autocomplete_search_input),
+                        childAtPosition(
+                                allOf(withId(R.id.autocomplete_fragment2),
+                                        childAtPosition(
+                                                withClassName(is("androidx.cardview.widget.CardView")),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        appCompatEditText3.perform(click());
+        Thread.sleep(2000);
+
+        ViewInteraction appCompatEditText4 = onView(
+                allOf(withId(com.google.android.libraries.places.R.id.places_autocomplete_search_bar),
+                        childAtPosition(
+                                allOf(withId(com.google.android.libraries.places.R.id.places_autocomplete_search_bar_container),
+                                        childAtPosition(
+                                                withId(com.google.android.libraries.places.R.id.places_autocomplete_content),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        appCompatEditText4.perform(replaceText("Viale Zara"), closeSoftKeyboard());
+        Thread.sleep(2000);
+
+        ViewInteraction recyclerView2 = onView(
+                allOf(withId(com.google.android.libraries.places.R.id.places_autocomplete_list),
+                        childAtPosition(
+                                withId(com.google.android.libraries.places.R.id.places_autocomplete_content),
+                                3)));
+        recyclerView2.perform(actionOnItemAtPosition(0, click()));
+        Thread.sleep(2000);
+
+        ViewInteraction materialButton3 = onView(
+                allOf(withId(R.id.buttonFind), withText("Cerca"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.fragment_container_main),
+                                        0),
+                                2),
+                        isDisplayed()));
+        materialButton3.perform(click());
+        Thread.sleep(4000);
+
+        ViewInteraction materialButton4 = onView(
+                allOf(withId(R.id.buttonNavigate), withText("Apri Mappa"),
+                        childAtPosition(
+                                allOf(withId(R.id.relativeLayoutCardRoute),
+                                        childAtPosition(
+                                                withId(R.id.route_card),
+                                                0)),
+                                2),
+                        isDisplayed()));
+        materialButton4.perform(click());
+        Thread.sleep(2000);
+
+        ViewInteraction view = onView(
+                allOf(withContentDescription("Google Maps"),
+                        withParent(withParent(withId(R.id.id_map))),
+                        isDisplayed()));
+        view.check(matches(isDisplayed()));
+
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withId(R.id.backButton),
+                        childAtPosition(
+                                allOf(withId(R.id.toolbarTop),
+                                        childAtPosition(
+                                                withId(R.id.appBarLayoutTop),
+                                                0)),
+                                0),
+                        isDisplayed()));
+        appCompatImageButton.perform(click());
+        Thread.sleep(2000);
+
+        ViewInteraction materialButton5 = onView(
+                allOf(withId(R.id.buttonSelect), withText("Seleziona"),
+                        childAtPosition(
+                                allOf(withId(R.id.relativeLayoutCardRoute),
+                                        childAtPosition(
+                                                withId(R.id.route_card),
+                                                0)),
+                                3),
+                        isDisplayed()));
+        materialButton5.perform(click());
+
+        ViewInteraction frameLayout = onView(
+                allOf(withParent(allOf(withId(R.id.fragment_container_main),
+                                withParent(withId(R.id.fragment_container_main)))),
+                        isDisplayed()));
+        frameLayout.check(matches(isDisplayed()));
+    }
+
+     */
+
+    private static Matcher<View> childAtPosition(
             final Matcher<View> parentMatcher, final int position) {
 
         return new TypeSafeMatcher<View>() {
@@ -265,6 +540,21 @@ public class NavigatorFragmentTest {
                 ViewParent parent = view.getParent();
                 return parent instanceof ViewGroup && parentMatcher.matches(parent)
                         && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+    public static Matcher<View> firstChildOf(final Matcher<View> parentMatcher) {
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("with first child view of type parentMatcher");
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && ((ViewGroup) parent).getChildAt(0).equals(view);
             }
         };
     }
